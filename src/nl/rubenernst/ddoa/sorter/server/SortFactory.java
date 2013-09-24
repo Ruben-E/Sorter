dddc05715856a7f5e42eaefb622be9c132fe0a47
Ruben Ernst <rubenernst@gmail.com>
2013-09-24 16:10:24 +0200

    Implemented SortFactory

--- src/nl/rubenernst/ddoa/sorter/server/SortFactory.java
+++ src/nl/rubenernst/ddoa/sorter/server/SortFactory.java
@@ -9,6 +9,18 @@ public class SortFactory implements ISortFactory {
 
     @Override
     public ISorter buildSorter(SortType type) {
-        return null;
+        ISorter sorter = null;
+        switch (type) {
+            case BUBBLESORT:
+                sorter = new BubbleSort();
+                break;
+            case QUICKSORT:
+                sorter = new QuickSort();
+                break;
+            default:
+                break;
+        }
+
+        return sorter;
     }
 }
