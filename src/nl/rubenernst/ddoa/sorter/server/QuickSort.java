5db8ce131661bb395899bf50d0e8582746780fde
Ruben Ernst <rubenernst@gmail.com>
2013-09-24 15:18:54 +0200

    Created QuickSort class

---
+++ src/nl/rubenernst/ddoa/sorter/server/QuickSort.java
@@ -0,0 +1,18 @@
+package nl.rubenernst.ddoa.sorter.server;
+
+/**
+ * User: rubenernst
+ * Date: 9/24/13
+ * Time: 3:16 PM
+ */
+public class QuickSort implements ISorter {
+
+    public QuickSort() {
+
+    }
+
+    @Override
+    public Comparable[] sort(Comparable[] comparables) {
+        return new Comparable[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
