cff77cb9e7d1d29490ba75765fce59d596eb1ad2
Ruben Ernst <rubenernst@gmail.com>
2013-09-24 15:34:19 +0200

    Implemented QuickSort algorithm

--- src/nl/rubenernst/ddoa/sorter/server/QuickSort.java
+++ src/nl/rubenernst/ddoa/sorter/server/QuickSort.java
@@ -11,8 +11,45 @@ public class QuickSort implements ISorter {
 
     }
 
+    /**
+     * @source http://thilinasameera.wordpress.com/2011/06/01/sorting-algorithms-sample-codes-on-java-c-and-matlab/ (2013, 24 september)
+     * @param comparables
+     * @return
+     */
     @Override
     public Comparable[] sort(Comparable[] comparables) {
-        return new Comparable[0];  //To change body of implemented methods use File | Settings | File Templates.
+        int lenD = comparables.length;
+        Comparable pivot = 0;
+        int ind = lenD / 2;
+        int i, j = 0, k = 0;
+        if (lenD < 2) {
+            return comparables;
+        } else {
+            Comparable[] L = new Comparable[lenD];
+            Comparable[] R = new Comparable[lenD];
+            Comparable[] sorted = new Comparable[lenD];
+            pivot = comparables[ind];
+            for (i = 0; i < lenD; i++) {
+                if (i != ind) {
+                    if (comparables[i].compareTo(pivot) == -1) {
+                        L[j] = comparables[i];
+                        j++;
+                    } else {
+                        R[k] = comparables[i];
+                        k++;
+                    }
+                }
+            }
+            Comparable[] sortedL = new Comparable[j];
+            Comparable[] sortedR = new Comparable[k];
+            System.arraycopy(L, 0, sortedL, 0, j);
+            System.arraycopy(R, 0, sortedR, 0, k);
+            sortedL = sort(sortedL);
+            sortedR = sort(sortedR);
+            System.arraycopy(sortedL, 0, sorted, 0, j);
+            sorted[j] = pivot;
+            System.arraycopy(sortedR, 0, sorted, j + 1, k);
+            return sorted;
+        }
     }
 }
