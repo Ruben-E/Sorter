978614eca046418053fafea4705f1fd25698f03d
Ruben Ernst <rubenernst@gmail.com>
2013-09-24 16:07:45 +0200

    Created tests for SortFactory

---
+++ test/nl/rubenernst/ddoa/sorter/server/SortFactoryTest.java
@@ -0,0 +1,31 @@
+package nl.rubenernst.ddoa.sorter.server;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * User: rubenernst
+ * Date: 9/24/13
+ * Time: 4:02 PM
+ */
+public class SortFactoryTest {
+    @Test
+    public void testBuildSorterBubbleSort() throws Exception {
+        SortType type = SortType.BUBBLESORT;
+
+        SortFactory sortFactory = new SortFactory();
+        ISorter sorter = sortFactory.buildSorter(type);
+
+        assertEquals(sorter.getClass(), BubbleSort.class);
+    }
+
+    @Test
+    public void testBuildSorterQuickSort() throws Exception {
+        SortType type = SortType.QUICKSORT;
+
+        SortFactory sortFactory = new SortFactory();
+        ISorter sorter = sortFactory.buildSorter(type);
+
+        assertEquals(sorter.getClass(), QuickSort.class);
+    }
+}
