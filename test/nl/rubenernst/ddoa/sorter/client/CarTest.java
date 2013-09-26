0ad0297f1de641e23fb9a75f6cd01165259f7922
Ruben Ernst <rubenernst@gmail.com>
2013-09-26 15:35:29 +0200

    Implemented CarTests

--- test/nl/rubenernst/ddoa/sorter/client/CarTest.java
+++ test/nl/rubenernst/ddoa/sorter/client/CarTest.java
@@ -1,6 +1,9 @@
 package nl.rubenernst.ddoa.sorter.client;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * User: rubenernst
@@ -10,46 +13,86 @@ import org.junit.Test;
 public class CarTest {
     @Test
     public void testGetBrand() throws Exception {
+        Car car = new Car();
+        car.setBrand("Audi");
 
+        assertEquals("Audi", car.getBrand());
     }
 
     @Test
     public void testSetBrand() throws Exception {
+        Car car = new Car();
+        car.setBrand("Audi");
 
+        assertEquals("Audi", car.getBrand());
     }
 
     @Test
     public void testGetModel() throws Exception {
+        Car car = new Car();
+        car.setModel("A4");
 
+        assertEquals("A4", car.getModel());
     }
 
     @Test
     public void testSetModel() throws Exception {
+        Car car = new Car();
+        car.setModel("A4");
 
+        assertEquals("A4", car.getModel());
     }
 
     @Test
     public void testGetTopSpeed() throws Exception {
+        Car car = new Car();
+        car.setTopSpeed(230);
 
+        assertEquals(230, car.getTopSpeed());
     }
 
     @Test
     public void testSetTopSpeed() throws Exception {
+        Car car = new Car();
+        car.setTopSpeed(230);
 
+        assertEquals(230, car.getTopSpeed());
     }
 
     @Test
     public void testGetAcceleration() throws Exception {
+        Car car = new Car();
+        car.setAcceleration(7.5f);
 
+        assertEquals(7.5f, car.getAcceleration(), 0);
     }
 
     @Test
     public void testSetAcceleration() throws Exception {
+        Car car = new Car();
+        car.setAcceleration(7.5f);
 
+        assertEquals(7.5f, car.getAcceleration(), 0);
     }
 
     @Test
     public void testCompareTo() throws Exception {
+        Car car1 = new Car();
+        Car car2 = new Car();
 
+        car1.setTopSpeed(230);
+        car2.setTopSpeed(230);
+
+        assertTrue(car1.compareTo(car2) == 0);
+
+        car1.setTopSpeed(230);
+        car2.setTopSpeed(200);
+
+        assertTrue(car1.compareTo(car2) >= 1);
+
+        car1.setTopSpeed(200);
+        car2.setTopSpeed(230);
+
+        assertTrue(car1.compareTo(car2) >= -1);
     }
 }
