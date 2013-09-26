1c2caf3fdbd5f0051c5566a412458c79d72d7f61
Ruben Ernst <rubenernst@gmail.com>
2013-09-26 17:12:59 +0200

    Created ListSplicer

---
+++ src/nl/rubenernst/ddoa/sorter/client/ListSplicer.java
@@ -0,0 +1,12 @@
+package nl.rubenernst.ddoa.sorter.client;
+
+/**
+ * User: rubenernst
+ * Date: 9/26/13
+ * Time: 5:10 PM
+ */
+public class ListSplicer {
+    public static Comparable[][] splice(Comparable[] list) {
+        return null;
+    }
+}
