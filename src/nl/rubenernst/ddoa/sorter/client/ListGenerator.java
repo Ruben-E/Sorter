1ec06a283676588c18af00ca8a2ba250c8e67931
Ruben Ernst <rubenernst@gmail.com>
2013-09-26 15:17:08 +0200

    Implemented dynamic method call in listGenerator, also created the tests for that method.

--- src/nl/rubenernst/ddoa/sorter/client/ListGenerator.java
+++ src/nl/rubenernst/ddoa/sorter/client/ListGenerator.java
@@ -1,5 +1,8 @@
 package nl.rubenernst.ddoa.sorter.client;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * User: rubenernst
  * Date: 9/25/13
@@ -11,19 +14,61 @@ public class ListGenerator {
 
 
     public static Comparable[] generateRandomIntegers(int length) {
-        if(length <= 0 || length > Integer.MAX_VALUE) {
+        if (length <= 0 || length > Integer.MAX_VALUE) {
             throw new IllegalArgumentException();
         }
 
         Comparable[] list = new Comparable[length];
-        for(int i = 0; i < length; i++) {
-            list[i] = MIN_INT + (int)(Math.random() * ((MAX_INT - MIN_INT) + 1));
+        for (int i = 0; i < length; i++) {
+            list[i] = MIN_INT + (int) (Math.random() * ((MAX_INT - MIN_INT) + 1));
         }
 
         return list;
     }
 
     public static Comparable[] generateRandomStrings(int length) {
+        if (length <= 0 || length > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException();
+        }
+
+        Comparable[] list = new Comparable[length];
+        for (int i = 0; i < length; i++) {
+            String randomWord = "";
+            for (int j = 0; j < Math.random() * 30; j++) {
+                int characterCode = 65 + (int) (Math.random() * ((122 - 65) + 1));
+                String character = Character.toString((char) characterCode);
+                randomWord += character;
+            }
+            list[i] = randomWord;
+        }
+
+        return list;
+    }
+
+    public static Comparable[] generateRandomList(Class type, int length) {
+        String className = type.getSimpleName();
+
+        try {
+            Class[] paramInt = new Class[1];
+            paramInt[0] = Integer.TYPE;
+
+            Class<?> cls;
+            cls = Class.forName("nl.rubenernst.ddoa.sorter.client.ListGenerator");
+
+            Method method = cls.getMethod("generateRandom" + className + "s", paramInt);
+
+            return (Comparable[]) method.invoke(null, length);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 }
--- test/nl/rubenernst/ddoa/sorter/client/ListGeneratorTest.java
+++ test/nl/rubenernst/ddoa/sorter/client/ListGeneratorTest.java
@@ -1,6 +1,7 @@
 package nl.rubenernst.ddoa.sorter.client;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
@@ -12,36 +13,14 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 public class ListGeneratorTest {
     @Test
     public void testGenerateRandomIntegers() throws Exception {
-        Comparable[] random = ListGenerator.generateRandomIntegers(10);
-        assertEquals(10, random.length);
+        int[] arrayLengths = {10, 100, 1000, 10000};
+        for (int arrayLength : arrayLengths) {
+            Comparable[] random = ListGenerator.generateRandomIntegers(arrayLength);
+            assertEquals(arrayLength, random.length);
 
-        for(int i = 0; i < random.length; i++) {
-            Comparable value = random[i];
-            assertThat(value, instanceOf(Integer.class));
-        }
-
-        Comparable[] random2 = ListGenerator.generateRandomIntegers(100);
-        assertEquals(100, random2.length);
-
-        for(int i = 0; i < random2.length; i++) {
-            Comparable value = random2[i];
-            assertThat(value, instanceOf(Integer.class));
-        }
-
-        Comparable[] random3 = ListGenerator.generateRandomIntegers(1000);
-        assertEquals(1000, random3.length);
-
-        for(int i = 0; i < random3.length; i++) {
-            Comparable value = random3[i];
-            assertThat(value, instanceOf(Integer.class));
-        }
-
-        Comparable[] random4 = ListGenerator.generateRandomIntegers(10000);
-        assertEquals(10000, random4.length);
-
-        for(int i = 0; i < random4.length; i++) {
-            Comparable value = random4[i];
-            assertThat(value, instanceOf(Integer.class));
+            for (Comparable value : random) {
+                assertThat(value, instanceOf(Integer.class));
+            }
         }
     }
 
@@ -57,6 +36,42 @@ public class ListGeneratorTest {
 
     @Test
     public void testGenerateRandomStrings() throws Exception {
+        int[] arrayLengths = {10, 100, 1000, 10000};
+        for (int arrayLength : arrayLengths) {
+            Comparable[] random = ListGenerator.generateRandomStrings(arrayLength);
+            assertEquals(arrayLength, random.length);
 
+            for (Comparable value : random) {
+                assertThat(value, instanceOf(String.class));
+                assertNotSame(value, "");
+                assertNotSame(value, " ");
+            }
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateRandomStringNegative() throws Exception {
+        ListGenerator.generateRandomStrings(-10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateRandomStringZero() throws Exception {
+        ListGenerator.generateRandomStrings(0);
+    }
+
+    @Test
+    public void testGenerateRandomList() throws Exception {
+        Class[] randomItemClasses = {String.class, Integer.class};
+        int[] arrayLengths = {10, 100, 1000, 10000};
+        for (Class itemClass : randomItemClasses) {
+            for (int arrayLength : arrayLengths) {
+                Comparable[] random = ListGenerator.generateRandomList(itemClass, arrayLength);
+                assertEquals(arrayLength, random.length);
+
+                for (Comparable value : random) {
+                    assertThat(value, instanceOf(itemClass));
+                }
+            }
+        }
     }
 }
