aa96354c3847fbb2ff644aaf25569e93b4afd845
Ruben Ernst <rubenernst@gmail.com>
2013-09-26 15:27:33 +0200

    Created CarTest

---
+++ test/nl/rubenernst/ddoa/sorter/client/CarTest.java
@@ -0,0 +1,55 @@
+package nl.rubenernst.ddoa.sorter.client;
+
+import org.junit.Test;
+
+/**
+ * User: rubenernst
+ * Date: 9/26/13
+ * Time: 3:27 PM
+ */
+public class CarTest {
+    @Test
+    public void testGetBrand() throws Exception {
+
+    }
+
+    @Test
+    public void testSetBrand() throws Exception {
+
+    }
+
+    @Test
+    public void testGetModel() throws Exception {
+
+    }
+
+    @Test
+    public void testSetModel() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTopSpeed() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTopSpeed() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAcceleration() throws Exception {
+
+    }
+
+    @Test
+    public void testSetAcceleration() throws Exception {
+
+    }
+
+    @Test
+    public void testCompareTo() throws Exception {
+
+    }
+}
