0f934aa3232cadb9688adcf2b528929b6b7e89b3
Ruben Ernst <rubenernst@gmail.com>
2013-09-30 14:35:30 +0200

    Refactored ListMerger

--- src/nl/rubenernst/ddoa/sorter/client/ListMerger.java
+++ src/nl/rubenernst/ddoa/sorter/client/ListMerger.java
@@ -1,14 +1,5 @@
 package nl.rubenernst.ddoa.sorter.client;
 
-import nl.rubenernst.ddoa.sorter.server.ISortFactory;
-import nl.rubenernst.ddoa.sorter.server.ISorter;
-import nl.rubenernst.ddoa.sorter.server.SortType;
-
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-
 /**
  * User: rubenernst
  * Date: 9/28/13
@@ -23,9 +14,6 @@ public class ListMerger {
         this.lists = lists;
         this.list = new Comparable[this.totalItems()];
         this.lastMerged = 0;
-
-
-        //this.joinLists();
     }
 
     public Comparable[] merge() {
@@ -77,29 +65,6 @@ public class ListMerger {
 
         return merged;
     }
-
-    /*private void joinLists() {
-        int totalItemNumber = 0;
-        for(Comparable[] list : this.lists) {
-            int itemNumber = 0;
-            for(Comparable item : list) {
-                this.list[totalItemNumber] = item;
-                totalItemNumber++;
-                itemNumber++;
-            }
-        }
-    }*/
-
-    /*public Comparable[] merge() throws RemoteException, NotBoundException, MalformedURLException {
-        //TODO: Config file
-        ISortFactory sortFactory = (ISortFactory) Naming.lookup("//localhost:1099/sortfactory");
-        //TODO: Change to MergeSort;
-        ISorter sorter = sortFactory.buildSorter(SortType.QUICKSORT);
-
-        Comparable[] results = sorter.sort(this.list);
-
-        return results;
-    }*/
 
     private int totalItems() {
         int totalItems = 0;
