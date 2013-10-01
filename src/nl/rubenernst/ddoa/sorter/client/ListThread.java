4e18b7d9c377be638a30b03c37581c7660196562
Ruben Ernst <rubenernst@gmail.com>
2013-10-01 09:35:43 +0200

    Documented ListThread

--- src/nl/rubenernst/ddoa/sorter/client/ListThread.java
+++ src/nl/rubenernst/ddoa/sorter/client/ListThread.java
@@ -8,6 +8,8 @@ import java.rmi.RemoteException;
 import java.util.Observable;
 
 /**
+ * Class to sort a list in a single thread.
+ *
  * User: rubenernst
  * Date: 9/27/13
  * Time: 2:24 PM
@@ -17,6 +19,14 @@ public class ListThread extends Observable implements Runnable {
     private Comparable[] list;
     private int number;
 
+    /**
+     * Constructor for ListThread
+     *
+     * @param sortFactory   The SortFactory to generate a sorter
+     * @param list          The list to sort
+     * @param number        The number of the thread
+     * @throws RemoteException
+     */
     public ListThread(ISortFactory sortFactory, Comparable[] list, int number) throws RemoteException {
         SortType sortType = SortType.random();
 
@@ -25,6 +35,9 @@ public class ListThread extends Observable implements Runnable {
         this.number = number;
     }
 
+    /**
+     * Start the thread
+     */
     @Override
     public void run() {
         try {
@@ -37,6 +50,11 @@ public class ListThread extends Observable implements Runnable {
         }
     }
 
+    /**
+     * Get the number of the thread.
+     *
+     * @return the number of the thread.
+     */
     public int getNumber() {
         return number;
     }
