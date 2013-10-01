91c7fa6fbd5d03643013fd04b925ec526c75c18c
Ruben Ernst <rubenernst@gmail.com>
2013-10-01 10:03:14 +0200

    Documented SortType

--- src/nl/rubenernst/ddoa/sorter/server/SortType.java
+++ src/nl/rubenernst/ddoa/sorter/server/SortType.java
@@ -1,6 +1,8 @@
 package nl.rubenernst.ddoa.sorter.server;
 
 /**
+ * Enum for different sorttypes
+ * <p/>
  * User: rubenernst
  * Date: 9/24/13
  * Time: 3:53 PM
@@ -8,6 +10,11 @@ package nl.rubenernst.ddoa.sorter.server;
 public enum SortType {
     BUBBLESORT, QUICKSORT, MERGESORT;
 
+    /**
+     * Generate a random Enum for SortType
+     *
+     * @return Random generated SortType
+     */
     public static SortType random() {
         return values()[(int) (Math.random() * values().length)];
     }
