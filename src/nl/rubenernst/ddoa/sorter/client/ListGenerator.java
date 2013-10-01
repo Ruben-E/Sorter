475ced7866fcd19e372885bd62f19f4e5a973a11
Ruben Ernst <rubenernst@gmail.com>
2013-10-01 09:16:33 +0200

    Documented ListGenerator

--- src/nl/rubenernst/ddoa/sorter/client/ListGenerator.java
+++ src/nl/rubenernst/ddoa/sorter/client/ListGenerator.java
@@ -15,10 +15,20 @@ public class ListGenerator {
     final static int WORD_MIN_CHAR = 65;
     final static int WORD_MAX_CHAR = 122;
 
+    /**
+     * Generate a random integer
+     *
+     * @return A random Comparable integer
+     */
     public static Comparable generateRandomInteger() {
         return INTEGER_MIN_VALUE + (int) (Math.random() * ((INTEGER_MAX_VALUE - INTEGER_MIN_VALUE) + 1));
     }
 
+    /**
+     * Generate a random string
+     *
+     * @return A random Comparable String
+     */
     public static Comparable generateRandomString() {
         String randomWord = "";
         for (int i = 0; i < Math.random() * 30; i++) {
@@ -30,6 +40,11 @@ public class ListGenerator {
         return (Comparable) randomWord;
     }
 
+    /**
+     * Generate a random Car
+     *
+     * @return A random Comparable car
+     */
     public static Comparable generateRandomCar() {
         Car car = new Car();
         car.setTopSpeed(CAR_MIN_TOPSPEED + (int) (Math.random() * ((CAR_MAX_TOPSPEED - CAR_MIN_TOPSPEED) + 1)));
@@ -38,6 +53,14 @@ public class ListGenerator {
         return (Comparable) car;
     }
 
+    /**
+     * Generate a random list (array) of types, based on the type param.
+     *
+     * @param type      The array will be filled with random objects of this class.
+     * @param length    The length of the array, number of random objects.
+     * @return          Array with random objects of the type from the type parameter.
+     * @throws Exception
+     */
     public static Comparable[] generateRandomList(Class type, int length) throws Exception {
         if (length <= 0 || length > Integer.MAX_VALUE) {
             throw new IllegalArgumentException();
@@ -58,14 +81,35 @@ public class ListGenerator {
         return list;
     }
 
+    /**
+     * Generate an array with random cars
+     *
+     * @param length    Length of the array
+     * @return          Array with random cars
+     * @throws Exception
+     */
     public static Comparable[] generateRandomIntegers(int length) throws Exception {
         return ListGenerator.generateRandomList(Integer.class, length);
     }
 
+    /**
+     * Generate an array with random strings
+     *
+     * @param length    Length of the Array
+     * @return          Array with random strings
+     * @throws Exception
+     */
     public static Comparable[] generateRandomStrings(int length) throws Exception {
         return ListGenerator.generateRandomList(String.class, length);
     }
 
+    /**
+     * Generate an array with random cars
+     *
+     * @param length    Length of the Array
+     * @return          Array with random cars
+     * @throws Exception
+     */
     public static Comparable[] generateRandomCars(int length) throws Exception {
         return ListGenerator.generateRandomList(Car.class, length);
     }
