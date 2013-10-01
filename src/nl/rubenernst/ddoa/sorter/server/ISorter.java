dd9cac4b8de7df57240e3d83b6e4439ba9fa50a1
Ruben Ernst <rubenernst@gmail.com>
2013-10-01 09:44:29 +0200

    Documented ISorter

--- src/nl/rubenernst/ddoa/sorter/server/ISorter.java
+++ src/nl/rubenernst/ddoa/sorter/server/ISorter.java
@@ -3,10 +3,19 @@ package nl.rubenernst.ddoa.sorter.server;
 import java.rmi.*;
 
 /**
+ * ISorter interface
+ *
  * User: rubenernst
  * Date: 9/24/13
  * Time: 11:26 AM
  */
 public interface ISorter extends Remote {
+    /**
+     * Sort method that will be called to sort the array
+     *
+     * @param comparables   Array of comparables to sort
+     * @return              Sorted comparables array
+     * @throws RemoteException
+     */
     public Comparable[] sort(Comparable[] comparables) throws RemoteException;
 }
