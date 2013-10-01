a1de0d434516f0861af0e03f766dd444b1f2927e
Ruben Ernst <rubenernst@gmail.com>
2013-10-01 09:21:55 +0200

    Documented ListJoiner

--- src/nl/rubenernst/ddoa/sorter/client/ListJoiner.java
+++ src/nl/rubenernst/ddoa/sorter/client/ListJoiner.java
@@ -4,6 +4,8 @@ import java.util.Observable;
 import java.util.Observer;
 
 /**
+ * Class to join the lists (threads)
+ *
  * User: rubenernst
  * Date: 9/27/13
  * Time: 12:32 PM
@@ -13,6 +15,11 @@ public class ListJoiner extends Observable implements Observer {
     private Comparable[][] lists;
     private int threadCounter = 0;
 
+    /**
+     * Constructor for ListJoiner
+     *
+     * @param totalLists The number of lists (threads) are beeing generated
+     */
     public ListJoiner(int totalLists) {
         if (totalLists <= 0) {
             throw new IllegalArgumentException();
@@ -22,6 +29,12 @@ public class ListJoiner extends Observable implements Observer {
         this.lists = new Comparable[totalLists][];
     }
 
+    /**
+     * Observer for ListThread
+     *
+     * @param observable    ListThread instance that called the observer
+     * @param o             Object send to the observer from the observable
+     */
     @Override
     public void update(Observable observable, Object o) {
         ListThread listThread = (ListThread) observable;
@@ -39,12 +52,23 @@ public class ListJoiner extends Observable implements Observer {
         }
     }
 
+    /**
+     * Add a single sorted list to the collection of all sorted lists
+     *
+     * @param number    Number of the threads
+     * @param list      The sorted Comparable list to add to the collection
+     */
     private void addList(int number, Comparable[] list) {
         this.lists[number] = list;
 
         this.threadCounter++;
     }
 
+    /**
+     * Merge the collection of single lists to one merged ordered list
+     *
+     * @return The merged ordered Comparable list
+     */
     private Comparable[] mergeLists() {
         ListMerger listMerger = new ListMerger(this.lists);
         Comparable[] mergedList = new Comparable[0];
