78a143ee599a818c142beb834799217113e7a747
Ruben Ernst <rubenernst@gmail.com>
2013-10-01 09:46:39 +0200

    Documented ISortFactory

--- src/nl/rubenernst/ddoa/sorter/server/ISortFactory.java
+++ src/nl/rubenernst/ddoa/sorter/server/ISortFactory.java
@@ -3,10 +3,19 @@ package nl.rubenernst.ddoa.sorter.server;
 import java.rmi.*;
 
 /**
+ * ISortFactory interface
+ *
  * User: rubenernst
  * Date: 9/24/13
  * Time: 3:46 PM
  */
 public interface ISortFactory extends Remote {
+    /**
+     * Method to build a sorter based on the type
+     *
+     * @param type  Generate sorter class of this type
+     * @return      Sorter class
+     * @throws RemoteException
+     */
     public ISorter buildSorter(SortType type) throws RemoteException;
 }
