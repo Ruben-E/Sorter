d2c62b848dab173e609b32041e622bb4f693ecf2
Ruben Ernst <rubenernst@gmail.com>
2013-10-01 10:02:05 +0200

    Documented SortFactory

--- src/nl/rubenernst/ddoa/sorter/server/SortFactory.java
+++ src/nl/rubenernst/ddoa/sorter/server/SortFactory.java
@@ -4,16 +4,30 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 /**
+ * Factory to generate a sorter class
+ * <p/>
  * User: rubenernst
  * Date: 9/24/13
  * Time: 3:47 PM
  */
 public class SortFactory extends UnicastRemoteObject implements ISortFactory {
 
+    /**
+     * Constructor for SortFactory
+     *
+     * @throws RemoteException
+     */
     protected SortFactory() throws RemoteException {
         super();
     }
 
+    /**
+     * Generate a sorter class based on the type parameter
+     *
+     * @param type Generate sorter class of this type
+     * @return The generated sorter
+     * @throws RemoteException
+     */
     @Override
     public ISorter buildSorter(SortType type) throws RemoteException {
         ISorter sorter = null;
